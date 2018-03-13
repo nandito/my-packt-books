74bfd05055242bc2730a113ea1e09c64ca9cb95d
Nandor Biro <nandor.biro@digitalnatives.hu>
2018-03-13 18:17:11 +0100

    Replace typings with yarn @types

--- typings/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/request-promise/index.d.ts" />
-/// <reference path="modules/cheerio/index.d.ts" />
-/// <reference path="modules/request/index.d.ts" />
