01178634f22097c6751d48362bb28dc4331deddc
Nandor Biro <nandor.biro@digitalnatives.hu>
2018-03-24 21:04:45 +0100

    Use Listr for nice terminal output

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import request from 'request'
 import rp from 'request-promise'
+import Listr from 'listr'
 import saveDataFile from './modules/saveDataFile'
 import openMyEbooksPage from './modules/openMyEbooksPage'
 import scrapeBookData from './modules/scrapeBookData'
-import logTitle from './modules/logTitle'
 import loginToPackt from './modules/login'
 
 export const baseRp: request.RequestAPI<
@@ -14,15 +14,44 @@ export const baseRp: request.RequestAPI<
   jar: true
 })
 
-loginToPackt()
-  .then(openMyEbooksPage)
-  .then(scrapeBookData)
-  .then(saveDataFile)
-  .then((message) => {
-    console.log(message)
-    logTitle('Process finished')
-  })
-  .catch(error => {
-    console.log('Error', error)
-    logTitle('Process finished')
-  })
+const tasks = new Listr([
+  {
+    title: 'Login to Packt',
+    task: () => new Promise(resolve => {
+      loginToPackt()
+        .then(() => resolve())
+    })
+  },
+  {
+    title: 'Open My eBooks page',
+    task: ctx => new Promise(resolve => {
+      openMyEbooksPage()
+        .then(body => {
+          ctx.ebooksPageBody = body
+          return resolve()
+        })
+    })
+  },
+  {
+    title: 'Scrape book data',
+    task: ctx => new Promise(resolve => {
+      scrapeBookData(ctx.ebooksPageBody)
+        .then(data => {
+          ctx.booksData = data
+
+          return resolve()
+        })
+    })
+  },
+  {
+    title: 'Save data to file',
+    task: ctx => new Promise(resolve => {
+      saveDataFile(ctx.booksData)
+        .then(() => resolve())
+    })
+  },
+])
+
+tasks.run().catch(err => {
+  console.error(err)
+})
