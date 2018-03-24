4841aaa6b15c687606d1d52a0a631641c341d3e8
Nandor Biro <nandor.biro@digitalnatives.hu>
2018-03-24 17:10:26 +0100

    Extract scrapeBookData module

--- index.ts
+++ index.ts
@@ -1,13 +1,10 @@
 import request from 'request'
 import rp from 'request-promise'
-import cheerio from 'cheerio'
 import { saveDataFile } from './modules/data-file-saver'
 import openMyEbooksPage from './modules/openMyEbooksPage'
-import downloadCoverImage from './modules/cover-image-downloader'
+import scrapeBookData from './modules/scrapeBookData'
 import logTitle from './modules/title-logger'
 import loginToPackt from './modules/login'
-
-import { BASE_URL } from './constants'
 
 export const baseRp: request.RequestAPI<
   rp.RequestPromise,
@@ -19,7 +16,7 @@ export const baseRp: request.RequestAPI<
 
 loginToPackt()
   .then(openMyEbooksPage)
-  .then(scrape)
+  .then(scrapeBookData)
   .then(saveDataFile)
   .then((message) => {
     console.log(message)
@@ -29,46 +26,3 @@ loginToPackt()
     console.log('Error', error)
     logTitle('Process finished')
   })
-
-function scrape(body): Promise<Array<Book>> {
-  let $ = cheerio.load(body)
-  const productListLength = $('.product-line').length
-  console.log('productListLength: ', productListLength)
-  
-  const pageData : Array<Book> = []
-  let downloadedFiles = 0
-  let falseItems = 0
-
-  return new Promise((resolve) => {
-    $('.product-line').each((_, item) => {
-      const title = $(item).find('.title').text().trim()
-  
-      if (!title) {
-        falseItems += 1
-
-        return null
-      }
-  
-      const href = $(item).find('.product-thumbnail a').attr('href')
-      const link = BASE_URL + href
-      const category = href.split('/')[1]
-      const safeName = href.split('/')[2]
-      const coverUrl = $(item).find('.product-thumbnail img').attr('data-original')
-      
-      downloadCoverImage(coverUrl, safeName)
-        .then((coverImageSrc) => {
-          pageData.push({ title, link, category, coverImageSrc })
-          downloadedFiles += 1
-          console.log('Download success: ', downloadedFiles, title)
-
-          if (downloadedFiles === (productListLength - falseItems)) {
-            console.log('Download is finished!')
-            console.log('downloadedFiles: ', downloadedFiles)
-            console.log('falseItems: ', falseItems)
-            
-            resolve(pageData)
-          }
-        })
-    })
-  })
-}
